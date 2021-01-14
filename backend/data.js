9b97ba49c289a9ecadd75f27d8783e6f9c5a9c86
ahmed nasser <ahmednas197@gmail.com>
2021-01-14 20:20:18 +0200

    create backend and create node server.js

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = { 
+    products : [
+        {
+            _id:'1' , 
+            name : 'Nike Slim shirt',
+            category : 'Shirts' , 
+            image: '/images/p1.jpg', 
+            price : 120 , 
+            countInStock:10, 
+            brnad: 'Nike' , 
+            rating : '4.5' , 
+            numReviews :10 , 
+            description : 'high quality product'
+        },
+         {
+            _id:'2' , 
+            name : 'adidias Slim shirt',
+            category : 'Shirts' , 
+            image: '/images/p2.jpg', 
+            price : 110 , 
+            countInStock:20, 
+            brnad: 'adidas' , 
+            rating : '4' , 
+            numReviews :10 , 
+            description : 'high quality product'
+        },
+         {
+            _id:'3' , 
+            name : 'lacosta Slim shirt',
+            category : 'Shirts' , 
+            image: '/images/p3.jpg', 
+            price : 200 , 
+            countInStock:0, 
+            brnad: 'Nike' , 
+            rating : '3.5' , 
+            numReviews :20 , 
+            description : 'high quality product'
+        },
+         {
+            _id:'4' , 
+            name : 'Nike Slim pant',
+            category : 'Pants' , 
+            image: '/images/p4.jpg', 
+            price : 150 , 
+            countInStock:10, 
+            brnad: 'Nike' , 
+            rating : '4' , 
+            numReviews :5 , 
+            description : 'high quality product'
+        },
+         {
+            _id:'5' , 
+            name : 'puma Slim pant',
+            category : 'Pants' , 
+            image: '/images/p5.jpg', 
+            price : 67 , 
+            countInStock:5, 
+            brnad: 'puma' , 
+            rating : '4.5' , 
+            numReviews :10 , 
+            description : 'high quality product'
+        },
+         {
+            _id:'6' , 
+            name : 'adidas Slim pant',
+            category : 'Pants' , 
+            image: '/images/p6.jpg', 
+            price : 89 , 
+            countInStock:12, 
+            brnad: 'adidas' , 
+            rating : '4' , 
+            numReviews :10 , 
+            description : 'high quality product'
+        }
+    ]
+}
+
+export default data ; 
