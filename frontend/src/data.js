b301f3b5995322b8cd1a4fda28a2868529ea6144
ahmed nasser <ahmednas197@gmail.com>
2021-01-14 16:47:12 +0200

    add the frontend using create-react-app

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = { 
+    products : [
+        {
+            _id:'1' , 
+            name : 'Nike Slim shirt',
+            category : 'Shirts' , 
+            image: '/images/p1.jpg', 
+            price : 120 , 
+            brnad: 'Nike' , 
+            rating : '4.5' , 
+            numReviews :10 , 
+            description : 'high quality product'
+        },
+         {
+            _id:'2' , 
+            name : 'adidias Slim shirt',
+            category : 'Shirts' , 
+            image: '/images/p2.jpg', 
+            price : 110 , 
+            brnad: 'adidas' , 
+            rating : '4' , 
+            numReviews :10 , 
+            description : 'high quality product'
+        },
+         {
+            _id:'3' , 
+            name : 'lacosta Slim shirt',
+            category : 'Shirts' , 
+            image: '/images/p3.jpg', 
+            price : 200 , 
+            brnad: 'Nike' , 
+            rating : '3.5' , 
+            numReviews :20 , 
+            description : 'high quality product'
+        },
+         {
+            _id:'4' , 
+            name : 'Nike Slim pant',
+            category : 'Pants' , 
+            image: '/images/p4.jpg', 
+            price : 150 , 
+            brnad: 'Nike' , 
+            rating : '4' , 
+            numReviews :20 , 
+            description : 'high quality product'
+        },
+         {
+            _id:'5' , 
+            name : 'puma Slim pant',
+            category : 'Pants' , 
+            image: '/images/p5.jpg', 
+            price : 67 , 
+            brnad: 'puma' , 
+            rating : '4.5' , 
+            numReviews :10 , 
+            description : 'high quality product'
+        },
+         {
+            _id:'6' , 
+            name : 'adidas Slim pant',
+            category : 'Pants' , 
+            image: '/images/p6.jpg', 
+            price : 89 , 
+            brnad: 'adidas' , 
+            rating : '4' , 
+            numReviews :10 , 
+            description : 'high quality product'
+        }
+    ]
+}
+
+export default data ; 
